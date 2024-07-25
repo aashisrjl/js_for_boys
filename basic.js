40b8732a410ff2f7f03b6daa5885af5a2af88e37
Aashis Rijal <aashisrijal252@gmail.com>
2024-07-25 17:57:23 +0545

    day one and 2

---
+++ basic.js
@@ -0,0 +1,224 @@
+
+// function kusal(){
+//     const  kusal = "bhatta"
+//     console.log(kusal)
+// }
+// kusal()
+
+// console.log(kusal)
+
+// console.log(a) // undefined
+
+
+// {
+//     var a = 5
+//     let b = 1
+//     console.log(a+b) //6
+// }
+// console.log(a) // 5
+
+// // console.log(a+b)
+// const name = "kedar"
+// console.log(name)
+
+// name = "kusal"
+// console.log(name)
+
+// const a =5;
+// const b = 5.5;
+// const c = "kedar";
+// const d = true; // boollean
+// const e = null; // null
+// const f = undefined; // undefined
+// const g = [1,2,3,4,5,6,7,8]
+// console.log(g)
+
+// const object ={
+//     name : "kedar",
+//     age : 20,
+//     address : "kolkata",
+// }
+// console.table(object)
+
+// object.name = "dhungana"
+// console.table(object)
+
+// = < > <= >= 
+
+// for(i=0;i<5;i++){
+//     console.log(i)
+// }
+
+// const object = {
+//     age : 5,
+//     name : "kedar",
+//     address : "kolkata"
+
+// }
+// forEach((item)=>{
+
+// })
+
+
+// function add(e){
+//     const a = 5;
+//      const b = 10;
+//     console.log(a+b)
+// }
+// add()
+
+
+// const add = (a,b)=>{
+//   const c= a+b;
+//     console.log(c);
+// }
+// add(2,3);
+
+//  const add = (a,b)=>a+b;
+//  add(2,3)
+
+
+// try {
+//     const a = 5;
+//     const b = 0;
+//     if (b === 0) {
+//         throw new Error("Division by zero is not allowed.");
+//     }
+//     const c = a / b;
+//     console.log(c);
+// } catch (err) {
+//     console.log(err.message); // This will now execute for division by zero
+// }
+
+// math.random
+// const user = parseInt(prompt("Enter a guess"))
+// const  pc = Math.floor(Math.random()*9)
+
+// console.log("user:",user)
+// console.log("pc:",pc)
+
+// if(user == pc){
+//     console.log("you won")
+// }else{
+//     console.log("you lost")
+// }
+
+// const date  = new Date();
+
+// const hour = date.getHours();
+// const min = date.getMinutes();
+// const sec = date.getSeconds();
+// const day = date.getDay();
+// const month = date.getMonth()
+// const year = date.getFullYear()
+
+
+// console.log(year)
+
+// console.log(hour+"/"+ min + "/" + sec )
+// console.log(hour,min,sec)
+// console.log(hour+"-"+ min + "-" + sec )
+
+// console.log(`The hour is ${hour} and the minute is ${min} and second is ${sec}`)
+// console.log("The hour is" + hour + "and the minute is"+ min + " and the second is "+ sec)
+
+
+// json
+
+// {
+//     "name":"sagar",   
+//     'age': 20,
+// }
+// name = "kedar", age = "20"
+// {
+//     name: keadqr
+//     age : "20"
+// }
+
+// class Cat{
+//     constructor(name,age){
+//         this.name = name
+//         this.age = age
+//     }
+// }
+// const pussy = new Cat("meow",12)
+// console.log(pussy)
+
+// class newcat extends Cat{
+//     constructor(name,age,weight){
+//         super(name,age)
+//         this.weight = weight
+//     }
+
+// }
+// const pus = new newcat("meow",12,16)
+// console.log(pus)
+// resolve,reject
+
+// votter check 
+// const user ={
+//     name:"kedar",
+//     age:20,
+//     gender:"male"
+// }
+// const check = (info)=>{
+//     if(info.age<18){
+//         console.log("you are not eligible to vote")
+//         return
+//     }else if(info.age == 18){
+//          console.log("you are eligible to vote")
+//          return
+//     }else{
+//         console.log(`name: ${info.name} age: ${info.age} and gender: ${info.gender}`) // string literal
+//         console.log("you are eligible to vote")
+//         return
+//     }
+    
+// }
+// check(user)
+
+//array
+
+// let array = [1,2,3,4,5]
+// array.splice(2,3,1024,1025,2023,1025,2020)
+// console.log(array)
+// let fruit = ["banana","orange","apple"]
+
+// // array.forEach((num)=>{
+// //     console.log(num*2)
+// // })
+// //reduce arrqay to single value
+// let sum = array.reduce((a,b)=>{
+//    return a+b
+// })
+// console.log(sum)
+
+
+// console.log(array.slice(2,5))
+// console.log(array.length) // length
+// // array[0] // index 1
+// console.log(array.join("-")) 
+
+// for(i=0;i<array.length;i++){
+//     console.log(array[i])
+// }
+// array.push(6) // shift (first)
+// array.pop() // unshift first
+// console.log(array.reverse()) // sort ascending
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,46 @@
+// confirm
+//alert
+//success
+
+// let a = parseInt(prompt("enter a"))
+// let b = parseInt(prompt ("enter b"))
+// confirm("did you want to sum")
+// let c= parseInt(a+b)
+// alert(c)
+
+//dom 
+//document
+
+
+// const div =document.createElement('div')
+// document.body.prepend(div)
+// div.classList.add("kedar")
+// div.remove()
+// div.classList.toggle("kedar")
+
+
+const button = document.querySelector(".button")
+const kedar = document.querySelector(".kedar")
+// const btn = document.getElementById("button")
+// const btnn = document.querySelector(".button")
+// const btnnn = document.getElementsByTagName("button")
+
+// const changeColor = ()=>{
+    
+//     alert("click")
+// }
+// kedar.classList.remove("kedar")
+button.addEventListener("click",()=>{
+    if(kedar.style.backgroundColor == "yellow"){
+        kedar.style.backgroundColor ="white"
+        button.textContent = "TURN ON"
+    }else{
+        kedar.style.backgroundColor ="yellow";
+        button.textContent = "TURN OFF"
+    }
+})
+
+
+
+
+
